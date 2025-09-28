aac67378b0330e526f868c0db70a63982ee746ff
Jakub Madej <01197284@pw.edu.pl>
2025-09-28 18:16:23 +0200

    init

---
+++ src/main/java/com/niebo/quemus/QuemusApplication.java
@@ -0,0 +1,13 @@
+package com.niebo.quemus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuemusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuemusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niebo/quemus/QuemusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niebo.quemus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuemusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
