c478ca8c1a7b1116e401584ff34c645b0b9c3dac
Duży komp <madej.jakub27@gmail.com>
2025-11-15 13:10:28 +0100

    zmiana zasad

--- src/main/java/com/niebo/quemus/models/game/Game.java
+++ src/main/java/com/niebo/quemus/models/game/Game.java
@@ -35,7 +35,7 @@ public class Game {
     @JsonIgnore
     private Random random = new Random();
     @JsonIgnore
-    private int winCondition = 1;
+    private int winCondition = 9;
     
     public Game(boolean isOnline, long game_ID, int turnsLeft){
         this.turnsLeft = turnsLeft;
